e38166979ac78f49ef27e203660bb7c33b2420d1
Ray <m5chun@uwaterloo.ca>
2016-05-04 05:23:32 -0400

    Intial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function showPage(pageName) {
+    $(".header-wrapper").hide();
+    $("#" + pageName).show();
+}
+
+
+$( document ).ready(function() {
+    // $("#about").hide();
+    // $("#portfolio").hide();
+    // $("#contact").hide();
+    //
+    // $("#aboutLink").click(function() {
+    //     showPage("about");
+    // });
+    // $("#portfolioLink").click(function() {
+    //     showPage("portfolio");
+    // });
+    // $("#contactLink").click(function() {
+    //     showPage("contact");
+    // });
+});
